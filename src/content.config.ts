52bdba31a75ec42aa161769a4b63c02c6fb3549e
LeoSM_07 <69401930+LeoSM-07@users.noreply.github.com>
2025-07-29 12:55:35 -0700

    More Setup

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,12 +2,14 @@ import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
 
 const posts = defineCollection({
-  loader: glob({ pattern: "**/[^_]*.{md,mdx}", base: "./src/posts" }),
+  // Use underscore as a prefix for draft posts.
+  // Drafts will show in dev but be hidden in prod
+  loader:  glob({ pattern: import.meta.env.PROD ? "**/[^_]*.{md,mdx}" : "**/*.{md,mdx}", base: "./src/posts" }),
   schema: z.object({
     slug: z.string(),
     title: z.string(),
-    description: z.string(),
-    publishDate: z.date()
+    description: z.string().optional(),
+    publishDate: z.date(),
   })
 });
 
