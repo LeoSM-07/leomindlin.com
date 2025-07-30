18029283d599453e071bdf6a2d887f460a0ff96d
LeoSM_07 <69401930+LeoSM-07@users.noreply.github.com>
2025-07-29 19:21:40 -0700

    Few Improvements

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,6 +12,7 @@ import rehypeExternalLinks from 'rehype-external-links';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://leomindlin.com",
   vite: {
     plugins: [tailwindcss()]
   },
